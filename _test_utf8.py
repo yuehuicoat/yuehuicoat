b0810ae1ec7e730b39af7d95b6213a245dfbfa35
HK BAOSHUN TRADE LIMITED <support@baotongshun.com>
2026-09-14 16:47:23 +0800

    chore: remove test scratch files from tracking

--- _test_utf8.py
+++
@@ -1,5 +0,0 @@
-import sys
-content = sys.argv[1].encode('utf-8').decode('utf-8')
-with open(r'E:\香港\03\_test_utf8.txt', 'w', encoding='utf-8', newline='') as f:
-    f.write(content)
-print('OK, bytes:', len(content.encode('utf-8')))
